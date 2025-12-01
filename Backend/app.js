74b344aedddc6bd6892660f23eeac3946db54bd9
DeepMatrixOfficial <optitan75@gmail.com>
2025-12-02 00:21:46 +0530

    User Registration API Done

--- Backend/app.js
+++ Backend/app.js
@@ -1,13 +1,21 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const express  = require("express");
+const express = require("express");
 const cors = require("cors");
 const app = express();
+const connectDB = require("./db/db");
+const userRoutes = require("./routes/user.route");
+
+connectDB();
+
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.send("Hello from the backend");
+  res.send("Hello from the backend");
 });
 
+app.use("/users", userRoutes);
 
 module.exports = app;
---
+++ Backend/db/db.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+function connectDB(){
+    mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((error) => console.log(error));
+}
+
+module.exports = connectDB;
