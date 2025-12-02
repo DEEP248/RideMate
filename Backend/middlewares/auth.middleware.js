2b7607857216f71daa5150d21e3b4d0add7a2b29
DeepMatrixOfficial <optitan75@gmail.com>
2025-12-02 23:42:38 +0530

    CAPTAIN AUTH DONE

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const blackListTokenModel = require("../models/blackListToken.model");
 const jwt = require("jsonwebtoken");
+const captainModel = require("../models/captain.model");
 
 module.exports.authUser = async (req, res, next) => {
   try {
@@ -9,7 +10,9 @@ module.exports.authUser = async (req, res, next) => {
     if (!token) {
       return res.status(401).json({ message: "Unauthorized" });
     }
-    const isBlackListedToken = await blackListTokenModel.findOne({ token:token });
+    const isBlackListedToken = await blackListTokenModel.findOne({
+      token: token,
+    });
     if (isBlackListedToken) {
       return res.status(401).json({ message: "Unauthorized" });
     }
@@ -22,5 +25,31 @@ module.exports.authUser = async (req, res, next) => {
     return next();
   } catch (error) {
     return res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+module.exports.authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const isBlacklisted = await blackListTokenModel.findOne({ token: token });
+
+  if (isBlacklisted) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const captain = await captainModel.findById(decoded._id);
+    req.captain = captain;
+
+    return next();
+  } catch (err) {
+    console.log(err);
+
+    res.status(401).json({ message: "Unauthorized" });
   }
 };
