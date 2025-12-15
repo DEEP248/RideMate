66c8125e2569c60901c622502d76a253dda93d33
DEEP248 <deepdarji2002@gmail.com>
2025-12-16 00:34:03 +0530

    Create ride and map API Routes

--- Backend/app.js
+++ Backend/app.js
@@ -7,6 +7,8 @@ const cookieParser = require("cookie-parser");
 const connectDB = require("./db/db");
 const userRoutes = require("./routes/user.route");
 const captainRoutes = require("./routes/captain.route");
+const mapsRoutes = require("./routes/map.route");
+const rideRoutes = require("./routes/ride.route");
 
 connectDB();
 
@@ -21,5 +23,7 @@ app.get("/", (req, res) => {
 
 app.use("/users", userRoutes);
 app.use("/captains", captainRoutes);
+app.use("/maps", mapsRoutes);
+app.use("/rides", rideRoutes);
 
 module.exports = app;
