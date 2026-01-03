eccf35dd837a5816f722f5d38e64584023c84eba
DEEP248 <deepdarji2002@gmail.com>
2026-01-03 18:18:10 +0530

    update mongo url

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,12 @@ const rideRoutes = require("./routes/ride.route");
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // later replace with frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
